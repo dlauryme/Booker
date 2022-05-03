b0a34044868b22288f88b1e17e0fcac8e031667d
dlauryme <dlaury.me@gmail.com>
2022-05-03 15:13:12 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function init() {
+    window.scrollTo(0, 0);
+
+    document.querySelectorAll("div.return-top-button button").forEach(el => {
+        el.addEventListener('click', () => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        })
+    })
+
+    document.querySelectorAll("div#main-catalog button").forEach(el => {
+        el.addEventListener('click', () => {
+            document.querySelectorAll("div#catalog").forEach(el => {
+                if (el.classList.contains("catalog-disabled")) {
+                    el.classList.remove("catalog-disabled");
+                } else {
+                    el.classList.add("catalog-disabled");
+                }
+            })
+        })
+    })
+}
+
+window.onload = init;
